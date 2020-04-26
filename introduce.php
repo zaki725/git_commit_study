606bdc019af08573bab5fd6c661d6f4000ff7587
zaki725 <zaki725725725@gmail.com>
2020-04-27 00:28:16 +0900

    [Add introduce.php][講師紹介画面]
    講師紹介画面が見られるようになりました。

---
+++ introduce.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>プロサー</title>
+</head>
+<body>
+<h1>講師紹介</h1>
+<h2>むらっしゅ</h2>
+<p>マレーシアでIT企業やってます。</p>
+<a href='main.php'>プロサー紹介画面へ</a>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>プロサー</title>
+</head>
+<body>
+<h1>プロサー</h1>
+<p>最短最速でプログラミングをマスターしたいならプロサー</p>
+<a href='introduce.php'>講師紹介ページへ</a>
+</body>
+</html>
